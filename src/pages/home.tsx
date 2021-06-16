5b635f779f6f71347a35bc19de8b4d9105137dcb
eggs <550467116@.qq.com>
2021-06-16 16:39:37 +0800

    grid布局

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,6 +6,13 @@ export const Home = () => {
   return (
     <div className='home'>
       <header style={{backgroundImage:`url(${headerBg})`}}>你好</header>
+        <main>
+            <section className="section1">1</section>
+            <section className="section2">2</section>
+            <section className="section3">3</section>
+            <section className="section4">4</section>
+            <section className="section5">5</section>
+        </main>
     </div>
   );
 };
