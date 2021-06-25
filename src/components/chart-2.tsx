57477608d7d0b68d949f26ec650895fae3c4404f
eggs <550467116@.qq.com>
2021-06-25 16:59:48 +0800

    演示动态数据

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -6,9 +6,36 @@ import {createEchartOptions} from '../shared/create-echart-options';
 
 export const Chart2 = () => {
     const divRef = useRef(null);
+    const myChart = useRef(null);
+    const data = [
+        {name: '城关区公安局', 2011: 2, 2012: 3},
+        {name: '七里河区公安局', 2011: 2, 2012: 3},
+        {name: '西固区公安局', 2011: 2, 2012: 3},
+        {name: '安宁区公安局', 2011: 2, 2012: 3},
+        {name: '红古区公安局', 2011: 2, 2012: 3},
+        {name: '永登县公安局', 2011: 2, 2012: 3},
+        {name: '皋兰县公安局', 2011: 2, 2012: 3},
+        {name: '榆中县公安局', 2011: 2, 2012: 3},
+        {name: '新区公安局', 2011: 2, 2012: 3},
+    ];
     useEffect(() => {
-        const myChart = echarts.init(divRef.current);
-        myChart.setOption(createEchartOptions({
+        setInterval(() => {
+            const newData = [
+                {name: '城关区公安局', 2011: 2, 2012: Math.random() * 10},
+                {name: '七里河区公安局', 2011: 2, 2012: 3},
+                {name: '西固区公安局', 2011: 2, 2012: 3},
+                {name: '安宁区公安局', 2011: 2, 2012: 3},
+                {name: '红古区公安局', 2011: 2, 2012: 3},
+                {name: '永登县公安局', 2011: 2, 2012: 3},
+                {name: '皋兰县公安局', 2011: 2, 2012: 3},
+                {name: '榆中县公安局', 2011: 2, 2012: 3},
+                {name: '新区公安局', 2011: 2, 2012: 3},
+            ];
+            x(newData);
+        }, 1000);
+    }, []);
+    const x = (data) => {
+        myChart.current.setOption(createEchartOptions({
             ...baseEchartOptions,
             grid: {
                 x: px(100),
@@ -24,8 +51,7 @@ export const Chart2 = () => {
             },
             yAxis: {
                 type: 'category',
-                data: ['城关区公安局', '七里河区公安局', '西固区公安局', '安宁区公安局', '红古区公安局',
-                    '永登县公安局', '皋兰县公安局', '榆中县公安局', '新区公安局'],
+                data: data.map(i => i.name),
                 axisLabel: {
                     fontSize: px(12),
                     formatter(val) {
@@ -37,15 +63,19 @@ export const Chart2 = () => {
                 {
                     name: '2011年',
                     type: 'bar',
-                    data: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                    data: data.map(i => i[2011]),
                 },
                 {
                     name: '2012年',
                     type: 'bar',
-                    data: [2, 3, 4, 5, 6, 7, 8, 9, 10]
+                    data: data.map(i => i[2012]),
                 }
             ]
         }));
+    };
+    useEffect(() => {
+        myChart.current = echarts.init(divRef.current);
+        x(data);
     }, []);
     return (
         <div className="bordered 破获排名">
